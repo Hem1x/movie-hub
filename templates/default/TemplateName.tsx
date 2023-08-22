3929fa4c5460afec105b9d1806d63f3a89056aa4
Alex Avk <iwanov.vany-ivanov@yandex.ru>
2023-08-22 22:14:00 +0300

    project init

---
+++ templates/default/TemplateName.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './TemplateName.module.scss';
+
+interface TemplateNameProps {}
+
+const TemplateName = ({}: TemplateNameProps) => (
+  <div className={styles.templateName}>TemplateName Component</div>
+);
+
+export default TemplateName;
