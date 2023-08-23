b38eb8790807581506bec1158431e690c3b3070a
Alex Avk <iwanov.vany-ivanov@yandex.ru>
2023-08-23 03:32:33 +0300

    made banner

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Search from '../components/Search/Search';
+import Banner from '../components/Banner/Banner';
 
 const Home = () => {
   return (
     <div style={{ height: '100vh', flex: 1, overflow: 'auto' }}>
       <Search />
+      <Banner />
     </div>
   );
 };
